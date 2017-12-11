0b378f45ccbe0eb37d44c36b76be4d14753a2046
Nepomuk1999 <christoph.bauer@students.fhv.at>
2017-12-11 14:48:59 +0100

    setup

--- src/main/java/UltimateBallTerminator.java
+++ src/main/java/UltimateBallTerminator.java
@@ -1,4 +1,19 @@
-public class UltimateBallTerminator {
+import com.cyberbotics.webots.controller.DifferentialWheels;
 
+public class UltimateBallTerminator extends DifferentialWheels {
+
+
+    public UltimateBallTerminator() {
+
+    }
+
+    private void run() {
+    }
+
+
+    public static void main(String[] args) {
+        UltimateBallTerminator controller = new UltimateBallTerminator();
+        controller.run();
+    }
 
 }
